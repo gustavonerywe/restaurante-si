0c81a35a711003036516b6e63e17a166a60b6d74
gustavo <gustavonerybarbosa@gmail.com>
2024-08-27 12:56:18 -0300

    24-08-27::12-56 register customers

---
+++ app/static/js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function (){
+    document.querySelector('#return-icon').addEventListener('click', function (){
+        window.history.back();
+    });
+})
