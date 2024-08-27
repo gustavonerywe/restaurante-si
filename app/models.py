7eb6852bbe49cffb211c998f1219496cd207cd9e
gustavo <gustavonerybarbosa@gmail.com>
2024-08-27 12:56:18 -0300

    24-08-27::12-56 register customers

--- app/models.py
+++ app/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    items = models.ManyToManyField(MenuItem)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer.name
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.order.customer.name + ' - ' + self.item.name
+    
+class Table(models.Model):
+    number = models.IntegerField()
+    capacity = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.number)
+    
+class Reservation(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer.name + ' - ' + str(self.date) + ' ' + str(self.time)
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Feedback(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comments = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer.name + ' - ' + str(self.rating) + ' stars'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 def index(request):
     return render(request, 'index.html')
 
-# Create your views here.
+
+def customer(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customer')  
+    else:
+        form = CustomerForm()
+    
+    customers = Customer.objects.all() 
+    return render(request, 'customer.html', {'form': form, 'customers': customers})
