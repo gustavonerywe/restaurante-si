5d8353ae4c9571fef7a881c8e852de4458e2d68b
gustavo <gustavonerybarbosa@gmail.com>
2024-08-28 12:39:27 -0300

    24-08-28::12-39 crud to client

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,41 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
 document.addEventListener('DOMContentLoaded', function (){
-    document.querySelector('#return-icon').addEventListener('click', function (){
-        window.history.back();
-    });
+    document.querySelectorAll('.remove').forEach(function(button){
+        button.onclick = function(){
+            var id = this.getAttribute('data-customer')
+            console.log(id)
+            var url = `/delete_customer/${id}/`
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrftoken,
+                },
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log('data:', data);
+                if(data.success){
+                    document.getElementById('customer-' + id).remove();
+                }
+            });
+        }
+    })
 })
