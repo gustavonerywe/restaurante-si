13f4431eed0bd9f27217600e43a5c2cbea14f095
gustavo <gustavonerybarbosa@gmail.com>
2024-08-29 12:35:42 -0300

    24-08-29::12-35 item dynamic logic

--- app/urls.py
+++ app/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('REMOVIDO/', REMOVIDO, name='REMOVIDO'),
     path('delete_REMOVIDO/<int:REMOVIDO_id>/', delete_REMOVIDO, name='delete_REMOVIDO'),
     path('edit_REMOVIDO/<int:REMOVIDO_id>/', edit_REMOVIDO, name='edit_REMOVIDO'),
+    path('item/', items, name='items'),
+    path('delete_item/<int:item_id>/', delete_item, name='delete_item'),
+    path('edit_item/<int:item_id>/', edit_item, name='edit_item'),
 ]
