2ed59754f1b8955416a86ff0ffee9b32cc3ebef5
gustavo <gustavonerybarbosa@gmail.com>
2024-08-29 12:58:37 -0300

    24-08-09::12-58 salary field

--- app/models.py
+++ app/models.py
@@ -79,6 +79,7 @@ class Role(models.Model):
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
 
     def __str__(self):
